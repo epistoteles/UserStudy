3a53d42c92c8c634e9b9f22da2a846e2e87bc8b6
Korbinian Koch <korbinian-koch@web.de>
2022-04-28 00:05:00 -0400

    add description

--- study.py
+++ study.py
@@ -5,7 +5,6 @@ import streamlit as st
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpRequest
-import json
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
 SPREADSHEET_ID = "1S7YBK5AqAE9GPHdZEto9WTPGOyLHSBP5EgDTejlG8dU"
